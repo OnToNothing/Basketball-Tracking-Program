a6376c2fd2d530b37f51c2fb672f060ea68c1a2b
Yousuf <ykanan2004@gmail.com>
2024-03-07 10:03:08 -0500

    delete stuff

--- src/main/java/com/example/mysql/DatabaseDriverLoader.java
+++
@@ -1,9 +0,0 @@
-package com.example.mysql;
-
-public class DatabaseDriverLoader {
-
-    public static void loadDriver(String driverName) throws ClassNotFoundException {
-        Class.forName(driverName);
-        System.out.println("Driver loaded!");
-    }
-}
