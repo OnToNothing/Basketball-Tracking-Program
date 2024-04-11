d907b2df0cf7b7ee80ee401e796030344cd5eff4
Yousuf <ykanan2004@gmail.com>
2024-04-11 10:51:53 -0400

    electron working kinda

---
+++ basketballtrackingnew/renderer.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   document.querySelector('.add_player_button').addEventListener('click', function() {
+//     addPlayer();
+//   });
+
+//   // Other event listeners...
+// });
+
+// // The addPlayer function and other DOM manipulation functions go here.
